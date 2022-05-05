55b3363bec8d14cf8a691bf4192b9dafc99a62b1
AlessandroFulco <alessandro.fulco99@gmail.com>
2022-05-05 15:35:44 +0200

    creazione ciclo numeri fino a 100

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// scrivi un programma che stampi in console i numeri da 1 a 100
+
+
+
+// creare un ciclo che stampi i numeri da 1 a 100
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+}
+// determinare se il numero sia divisibile per 3(fizz), 5(buzz) oppure entrambi(fizzBuzz)
+
+// inserire gli elementi nella pagina html
+
+// applicare uno stile differente a seconda del valore dell'indice per i multipli di 3, 5 o entrambi
+
