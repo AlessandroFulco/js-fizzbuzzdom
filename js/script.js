72d94fcd44370b957494e78eb8b632cf1800e14b
AlessandroFulco <alessandro.fulco99@gmail.com>
2022-05-05 16:50:59 +0200

    assegnazione fizz, buzz, fizzbuzz

--- js/script.js
+++ js/script.js
@@ -3,10 +3,25 @@
 
 
 // creare un ciclo che stampi i numeri da 1 a 100
-for (let i = 1; i <= 100; i++) {
+let type;
+
+let myList = document.querySelector('ul.list');
+for (let i = 1; i <= 30; i++) {
     console.log(i);
+    const myListItem = document.createElement('li');
+    // determinare se il numero sia divisibile per 3(fizz), 5(buzz) oppure entrambi(fizzBuzz)
+    if (i % 3 === 0 && i % 5 === 0) {
+        type = "FizzBuzz";
+        myListItem.append(type);
+    } else if (i % 5 === 0) {
+        type = "Buzz";
+        myListItem.append(type);
+    } else if (i % 3 === 0) {
+        type = "Fizz";
+        myListItem.append(type);
+    }
+    myList.append(myListItem);
 }
-// determinare se il numero sia divisibile per 3(fizz), 5(buzz) oppure entrambi(fizzBuzz)
 
 // inserire gli elementi nella pagina html
 
